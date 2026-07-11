a56c1d7ffb7c4193e14a1ae0bad865bbb43167c6
dvaustria741-a11y <dvaustria741@gmail.com>
2026-07-11 23:05:39 +0800

    feat: add chunk keybind support to :setbind

--- src/main/java/me/constantindev/antiantixray/Commands/SetBind.java
+++ src/main/java/me/constantindev/antiantixray/Commands/SetBind.java
@@ -9,39 +9,40 @@ import java.io.IOException;
 
 public class SetBind extends Base {
     public SetBind() {
-        super("setbind", new String[]{"setbind", "sb", "bind"}, "Sets custom binds for either scanning or removing blocks");
+        super("setbind", new String[]{"setbind", "sb", "bind"}, "Sets custom binds for scanning, removing, or chunk scanning. Usage: :setbind <scan|remove|chunk> <key>");
     }
 
     @Override
     public void run(String[] args) {
         assert MinecraftClient.getInstance().player != null;
         if (args.length < 3) {
-
-            MinecraftClient.getInstance().player.sendMessage(Text.of("[AAX] Please provide the property you want to chage as 2nd argument and the key to change it to as 3rd argument."), false);
+            MinecraftClient.getInstance().player.sendMessage(
+                Text.of("[AAX] Usage: :setbind <scan|remove|chunk> <key>"), false);
             return;
         }
         int kc = args[2].toUpperCase().charAt(0);
         switch (args[1].toLowerCase()) {
             case "scan":
                 AntiAntiXray.rvn.setKeyCode(kc);
-                try {
-                    ConfigHelper.setScanKBToFile(kc);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                MinecraftClient.getInstance().player.sendMessage(Text.of("[AAX] Set scanning keybind to " + ((char) kc)), false);
+                try { ConfigHelper.setScanKBToFile(kc); } catch (IOException e) { e.printStackTrace(); }
+                MinecraftClient.getInstance().player.sendMessage(
+                    Text.of("[AAX] Set scan keybind to " + ((char) kc)), false);
                 break;
             case "remove":
                 AntiAntiXray.removeBlockBeta.setKeyCode(kc);
-                try {
-                    ConfigHelper.setRemoveKBToFile(kc);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                MinecraftClient.getInstance().player.sendMessage(Text.of("[AAX] Set removing keybind to " + ((char) kc)), false);
+                try { ConfigHelper.setRemoveKBToFile(kc); } catch (IOException e) { e.printStackTrace(); }
+                MinecraftClient.getInstance().player.sendMessage(
+                    Text.of("[AAX] Set remove keybind to " + ((char) kc)), false);
+                break;
+            case "chunk":
+                AntiAntiXray.chunkKey.setKeyCode(kc);
+                try { ConfigHelper.setChunkKBToFile(kc); } catch (IOException e) { e.printStackTrace(); }
+                MinecraftClient.getInstance().player.sendMessage(
+                    Text.of("[AAX] Set chunk scan keybind to " + ((char) kc)), false);
                 break;
             default:
-                MinecraftClient.getInstance().player.sendMessage(Text.of("[AAX] Invalid property. Please choose either \"scan\" or \"remove\"."), false);
+                MinecraftClient.getInstance().player.sendMessage(
+                    Text.of("[AAX] Invalid property. Choose: scan, remove, or chunk."), false);
                 return;
         }
         super.run(args);
